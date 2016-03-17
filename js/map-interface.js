4bfeb85400466172ae40e003aadc9741e5d4cb74
Joshua Gustafson and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-03-17 14:54:19 -0700

    be able to delete individual markers from map

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,16 +1,19 @@
 var mapFunctions = require("./../js/map.js");
 
+
+
+
 $(document).ready(function() {
 google.maps.event.addDomListener(window, 'load', mapFunctions.initMap);
   $("#submitLocation").click(function(event) {
-    event.preventDefault();
+      event.preventDefault();
     address = $("#inputLocation").val();
     mapFunctions.inputLocation(address);
   });
-  $("#delLast").click(function(event) {
-    event.preventDefault();
-    mapFunctions.deleteLastMarker();
-  });
+  // $("#delLast").click(function(event) {
+  //   event.preventDefault();
+  //   mapFunctions.deleteLastMarker();
+  // });
   $("#delAll").click(function(event) {
     event.preventDefault();
     mapFunctions.deleteMarkers();
--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var map;
 var markers = [];
 
 exports.initMap = function() {
-    map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
@@ -13,8 +13,10 @@ exports.initMap = function() {
 
 };
 
+
 exports.inputLocation = function(address) {
   var geocoder = new google.maps.Geocoder();
+  var location = event.latLng;
   geocoder.geocode( { 'address': address }, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       var latlng = results[0].geometry.location;
@@ -25,16 +27,37 @@ exports.inputLocation = function(address) {
       map = new google.maps.Map(document.getElementById("map"), mapOptions);
       var marker = new google.maps.Marker( {
         position: latlng,
-        icon: "./../img/face.png"
+        optimized:false,
+        icon: "./../img/bomb.gif",
+        title: "300 casualites"
       });
       marker.setMap(map);
       marker.setAnimation(google.maps.Animation.DROP);
       setTimeout(function() {
         marker.setAnimation(null);
       }, 2100);
+
       map.addListener('click', function(event) {
         exports.addMarker(event.latLng);
-  });
+
+    });
+
+
+    var infoWindow = new google.maps.InfoWindow( {
+      content: "<br /><input type = 'button' value = 'Delete' onclick = 'DeleteMarker(" + marker.id + ");' value = 'Delete' />"
+    });
+
+    google.maps.event.addListener(marker, "click", function(event) {
+
+      infoWindow.open(map, marker);
+    });
+
+
+
+      // google.maps.event.addListener(marker,'click',function() { // MAYBE CAN'T HAVE TWO CLICK LISTENERS AT ONCE, BUT THIS LETS YOU ZOOM ON A MARKER
+      //   map.setZoom(20);
+      //   map.setCenter(marker.getPosition());
+      // });
     } else {
       alert("Geocode was not successful for the following reason: " + status);
     }
@@ -44,22 +67,43 @@ exports.inputLocation = function(address) {
 exports.addMarker = function(location) {
   var marker = new google.maps.Marker({
     position: location,
-    icon: "./../img/face.png",
-    map: map
+    optimized:false,
+    icon: "./../img/bomb.gif",
+    map: map,
+    title: "300 casualites"
 
   });
+
   markers.push(marker);
   marker.setAnimation(google.maps.Animation.DROP);
+  marker.id = markers.length;
+
+
+
+  var infoWindow = new google.maps.InfoWindow( {
+    content: "<br />"+marker.id+"<input type = 'button' value = 'Delete' class='delLast' data-id='" + marker.id + "' value = 'Delete' />"
+  });
+
+  google.maps.event.addListener(marker, "click", function(event) {
+
+    infoWindow.open(map, marker);
+    $(".delLast").click(function() {
+      exports.deleteLastMarker($(this).attr('data-id'));
+    });
+  });
+
 };
 
- exports.deleteMarkers = function() {
+exports.deleteMarkers = function() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
   markers = [];
-}
-
- exports.deleteLastMarker = function() {
-  markers.pop();
-  exports.initMap();
-}
+};
+exports.deleteLastMarker = function(id) {
+  for (var i = 0; i < markers.length; i++) {
+    if (markers[i].id == id) {
+      markers[i].setMap(null);
+    }
+  }
+};
